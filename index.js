c0d9bf6dfecc9d457079047f438f1a35f3830a61
Julapat Julnual <julapat@set.or.th>
2018-06-03 08:55:02 +0700

    Update index

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const app = express()
 
-express()
+app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  //.get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => {
+    res.send('Hello world')
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
