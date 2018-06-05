e00aa24d1a178b02aa206c738e4a0b5e0626cd27
Julapat Julnual <julapat@set.or.th>
2018-06-05 07:57:22 +0700

    Change db to mongo

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ const app = express()
 const bodyParser = require('body-parser');
 // const sqlite3 = require('sqlite3').verbose()
 // const db = new sqlite3.Database('worldcup.db')
-const pgp = require('pg-promise')(/*options*/)
-const db = pgp('postgres://gxqvybbufxriks:677d29ca2039c13cb3b971ee3785d85b4a7193eb62bf7a503c53b3f6badc127b@ec2-107-20-249-68.compute-1.amazonaws.com:5432/d3fkcigkaqpmbp')
+// const pgp = require('pg-promise')(/*options*/)
+// const db = pgp('postgres://gxqvybbufxriks:677d29ca2039c13cb3b971ee3785d85b4a7193eb62bf7a503c53b3f6badc127b@ec2-107-20-249-68.compute-1.amazonaws.com:5432/d3fkcigkaqpmbp')
+
+const MongoClient = require('mongodb').MongoClient
+const db_url = 'mongodb://stt:Settrade99@ds247670.mlab.com:47670/stt-worldcup-2018'
 
 app
   .use(express.static(path.join(__dirname, 'public')))
@@ -51,13 +54,23 @@ app
     })
   })
   .get('/users', (req, res) => {
-    const sql = 'SELECT * FROM EMPLOYEE'
-    db.any(sql).then((data) => {
-      res.type('json').status(200).send(data)
+    MongoClient.connect(db_url, function (err, db) {
+      if (err) res.type('json').status(500).send(err)
+    
+      db.collection('users').find().toArray(function (err, result) {
+        if (err) res.type('json').status(500).send(err)
+    
+        res.type('json').status(200).send(result)
+      })
     })
-    .catch((error) => {
-      res.type('json').status(500).send(error)
-    })
+
+    // const sql = 'SELECT * FROM EMPLOYEE'
+    // db.any(sql).then((data) => {
+    //   res.type('json').status(200).send(data)
+    // })
+    // .catch((error) => {
+    //   res.type('json').status(500).send(error)
+    // })
   })
   .post('/add-user', (req, res) => {
     db.none('INSERT INTO EMPLOYEE(ID, NAME, POSITION) VALUES(${id}, ${name}, ${position}) ON CONFLICT(ID) DO UPDATE SET NAME=${name}, POSITION=${position}', req.body).then((data) => {
